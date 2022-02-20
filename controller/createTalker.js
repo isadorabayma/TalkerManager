8329d81aec4d91bee189ac0cd3d19ad7fa927de6
isadora <isadora.bayma@gmail.com>
2022-02-20 10:14:52 -0300

    create newtalker missing val

--- controller/createTalker.js
+++ controller/createTalker.js
@@ -4,16 +4,17 @@ const DATAFILE = 'talker.json';
 
 const create = async (req, res, next) => {
     try {
-        const data = await fs.readFile(DATAFILE);
+        const data = await fs.readFile(DATAFILE, 'utf8');
         const talkers = JSON.parse(data);
+        const lastId = talkers[talkers.length - 1].id;
 
-        const newTalker = req.body;
+        const newTalker = { id: lastId + 1, ...req.body };
         talkers.push(newTalker);
 
         const stringifiedTalkers = JSON.stringify(talkers);
         await fs.writeFile(DATAFILE, stringifiedTalkers);
 
-        return res.status(201).send({ newTalker });
+        return res.status(201).json(newTalker);
     } catch (e) {
         next(e);
     }
